4ca8b793082fb1d133d4baa0fb1591c64a7e2f15
Miguel Batista <miguel.batista@sky.uk>
2023-06-01 13:53:41 +0100

    DIContainer - replaced String with ObjectIdentifier

--- Modules/Sources/DI/DIContainer.swift
+++ Modules/Sources/DI/DIContainer.swift
@@ -1,19 +1,19 @@
 import Foundation
 
 public final class DIContainer: DIResolver {
-    private var registrations: [String: DIServiceInstanceRetrieving] = [:]
+    private var registrations: [ObjectIdentifier: DIServiceInstanceRetrieving] = [:]
 
     @discardableResult
     public func register<T: Any>(_ type: T.Type, factory: @escaping (DIResolver) -> T) -> DIService<T> {
         let service = DIService(factory: factory)
         service.container = self
-        registrations[String(describing: type)] = service
+        registrations[ObjectIdentifier(type)] = service
         return service
     }
 
     public func resolve<T: Any>(_ type: T.Type) -> T {
         guard
-            let service = registrations[String(describing: type)],
+            let service = registrations[ObjectIdentifier(type)],
             let instance = service.getInstance() as? T
         else {
             fatalError("Failed to resolve type '\(type)'")
