b9ad9039a20749f50e2ba6839f415f9089f5fe23
Miguel Batista <miguel.batista@sky.uk>
2023-06-01 11:09:05 +0100

    Synchronization improvements

--- Modules/Sources/DI/DIContainer.swift
+++ Modules/Sources/DI/DIContainer.swift
@@ -2,7 +2,6 @@ import Foundation
 
 public final class DIContainer: DIResolver {
     private var registrations: [String: DIServiceInstanceRetrieving] = [:]
-    private var lock: NSRecursiveLock = NSRecursiveLock()
 
     @discardableResult
     public func register<T: Any>(_ type: T.Type, factory: @escaping (DIResolver) -> T) -> DIService<T> {
@@ -13,19 +12,11 @@ public final class DIContainer: DIResolver {
     }
 
     public func resolve<T: Any>(_ type: T.Type) -> T {
-        guard let service = registrations[String(describing: type)] else {
-            fatalError("Failed to resolve '\(type)'")
-        }
-
-        let needsSynchronization = !Thread.isMainThread && service.scope == .singleton
-        if needsSynchronization {
-            lock.lock()
-        }
-        guard let instance = service.getInstance() as? T else {
-            fatalError("Failed to get instance for '\(type)'")
-        }
-        if needsSynchronization {
-            lock.unlock()
+        guard
+            let service = registrations[String(describing: type)],
+            let instance = service.getInstance() as? T
+        else {
+            fatalError("Failed to resolve type '\(type)'")
         }
         return instance
     }
--- Modules/Sources/DI/DIService.swift
+++ Modules/Sources/DI/DIService.swift
@@ -1,13 +1,13 @@
 import Foundation
 
 protocol DIServiceInstanceRetrieving {
-    var scope: DIServiceScope { get }
     func getInstance() -> Any
 }
 
 public final class DIService<T: Any>: DIServiceInstanceRetrieving {
+    private let lock: NSRecursiveLock = NSRecursiveLock()
     private let factory: (DIContainer) -> T
-    private(set) var scope: DIServiceScope
+    private var scope: DIServiceScope
     private var instance: T?
 
     weak var container: DIContainer?
@@ -26,12 +26,24 @@ public final class DIService<T: Any>: DIServiceInstanceRetrieving {
         case .standard:
             return factory(container)
         case .singleton:
+            let needsSynchronization = !Thread.isMainThread && scope == .singleton
+            if needsSynchronization {
+                lock.lock()
+            }
+
+            defer {
+                if needsSynchronization {
+                    lock.unlock()
+                }
+            }
+            
             if let existingInstance = instance {
                 return existingInstance
             }
 
             let newInstance = factory(container)
             instance = newInstance
+
             return newInstance
         }
     }
